f1ddbb60b6961c58ff6542a5a63f24498a915af0
Garrett Taiji <gfmusicman@gmail.com>
2013-01-28 22:31:54 -0800

    modified the app delegate header with some comments

--- RepoTest/AppDelegate.h
+++ RepoTest/AppDelegate.h
@@ -5,6 +5,7 @@
 //  Created by Garrett Taiji on 1/28/13.
 //  Copyright (c) 2013 Garrett Taiji. All rights reserved.
 //
+// Haven't done anything. just making some changes to play around with commiting to github
 
 #import <UIKit/UIKit.h>
 
